6cc9bb4965818b88c0227d41d87aa0386e6a6f3f
Talha <trinitro619@gmail.com>
2019-02-19 16:47:48 +0500

    add base.html

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts = [
+    {
+        'author': 'Salaza Slytherin',
+        'title': 'Blog Post 1',
+        'content': 'FIrst post contene',
+        'date_posted': 'August 27, 2018'
+    }
+
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
